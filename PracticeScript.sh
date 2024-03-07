e637c57e444b888d989e49dc79eb7b6f0a13789c
Ubuntu <ubuntu@ip-10-0-0-22nithinsystemmailnotify.org>
2024-03-07 21:34:49 +0000

    first commit1

---
+++ PracticeScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#'continue' statement in a loop
+for n in {1..10}; do
+if [[ $n -eq 2 || $n -eq 4 || $n -ge 7 ]]; then
+echo "Iteration skipped for n = $n"
+continue #Skipping rest of the code in this iteration
+fi
+echo "Iteration $n"
+done

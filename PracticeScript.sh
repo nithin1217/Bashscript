6fcbde26338c968b7f304260ffe4b424a228583f
nithin1217 <108689346+nithin1217@users.noreply.github.com>
2024-03-08 03:20:23 +0530

    Update PracticeScript.sh

--- PracticeScript.sh
+++ PracticeScript.sh
@@ -2,7 +2,7 @@
 
 #'continue' statement in a loop
 for n in {1..10}; do
-if [[ $n -eq 2 || $n -eq 4 || $n -ge 7 ]]; then
+if [[ $n -eq 2 || $n -eq 4 || $n -ge 8 ]]; then
 echo "Iteration skipped for n = $n"
 continue #Skipping rest of the code in this iteration
 fi
